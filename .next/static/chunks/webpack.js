1a12371b36aa98f006c4fbd6aa333e1e6ef2f0b9
Prashant830 <72181295+Prashant830@users.noreply.github.com>
2023-10-20 14:29:28 +0530

    added gitignore file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e256dadd77c1b13"; }
+/******/ 		__webpack_require__.h = function() { return "19b243f976f4e72c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
