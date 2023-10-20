75b8dfde83fb8e5505834a849d03e137db580d1e
Prashant830 <72181295+Prashant830@users.noreply.github.com>
2023-10-20 14:52:18 +0530

    added next at yarn

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19b243f976f4e72c"; }
+/******/ 		__webpack_require__.h = function() { return "d71e860502a6c144"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.6436be23f19c6d0f.hot-update.js
+++ .next/static/webpack/webpack.6436be23f19c6d0f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "56c73456c149de17"; }
+/******/ 	__webpack_require__.h = function() { return "e6f95cbb7fae5374"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
